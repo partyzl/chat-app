6c2216bb0e584bdfae17ba5a32957b2e4b080ba0
Parth Patel <parth6796@yahoo.com>
2021-10-19 13:34:28 +0100

    reused my message logic for their message

--- src/components/TheirMessage.js
+++ src/components/TheirMessage.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-const TheirMessage = () => {
-  return <div>Their Message</div>;
+const TheirMessage = ({ lastMsg, msg }) => {
+  const isFirstMsgByUser =
+    !lastMsg || lastMsg.sender.username !== msg.sender.username;
+
+  return (
+    <div className="message-row">
+      {isFirstMsgByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${msg?.sender?.avatar})` }}
+        />
+      )}
+      {msg?.attachments?.length > 0 ? (
+        <img
+          src={msg.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: "right" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "right",
+            marginRight: "18px",
+            color: "white",
+            backgroundColor: "#3b2a50",
+          }}
+        >
+          {msg.text}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TheirMessage;
