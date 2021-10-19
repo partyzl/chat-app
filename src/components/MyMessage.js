d9a3cca5918734ba904bd0980c1009df2548bd1f
Parth Patel <parth6796@yahoo.com>
2021-10-19 13:28:19 +0100

    added logic for my messages

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const MyMessage = () => {
-  return <div>My Messages</div>;
+const MyMessage = ({ msg }) => {
+  if (msg?.attachments?.length > 0) {
+    return (
+      <img
+        src={msg.attachments[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3b2a50",
+      }}
+    >
+      {msg.text}
+    </div>
+  );
 };
 
 export default MyMessage;
